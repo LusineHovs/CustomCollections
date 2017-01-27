95283994b741bdfae19af22ccf4d5dd2b970bdc2
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-27 17:09:14 +0400

    Additions

---
+++ FibonacciSequence/Yield/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in UserCollection.PowerofYield())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FibonacciSequence/Yield/UserCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield
+{
+    class UserCollection
+    {
+        public static IEnumerable PowerofYield()
+        {
+            string s = "Hello world";
+            char[] a = s.ToArray();
+            foreach (var item in a)
+            {
+                yield return item;
+            }
+        }
+    }
+}
