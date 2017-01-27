71dfe30dd8e7ee29a62054dec469c959c4472b26
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-27 15:30:15 +0400

    Addition

---
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        public static IEnumerable<int> GetFibonacciNumber()
+        {
+            int previous = 0;
+            int current = 1;
+
+            yield return current;
+
+            for (int i = 0; i < 45; i++)
+            {
+                int next = previous + current;
+                previous = current;
+                current = next;
+                yield return next;
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach (var item in GetFibonacciNumber().Take(10))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
